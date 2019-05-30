c5d1f90e59b52a6a5a85072a2fe9b2593179559a
Udai Anand <udaianand.m@gmail.com>
2019-05-30 11:58:33 +0530

    setup

---
+++ src/main/java/features/login.feature
@@ -0,0 +1,10 @@
+Feature: Login feature
+
+Scenario: Login Test Scenario
+
+Given user is already on login page
+When title of login page is CRM
+Then user enters username and password
+Then user clicks on login button
+Then user in on homepage
+Then user closes the browser
---
+++ src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -0,0 +1,44 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginStepDefinition {
+
+	@Given("^user is already on login page$")
+	public void user_is_already_on_login_page() {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@When("^title of login page is CRM$")
+	public void title_of_login_page_is_CRM() {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@Then("^user enters username and password$")
+	public void user_enters_username_and_password() {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@Then("^user clicks on login button$")
+	public void user_clicks_on_login_button() {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@Then("^user in on homepage$")
+	public void user_in_on_homepage() {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@Then("^user closes the browser$")
+	public void user_closes_the_browser() {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+}
